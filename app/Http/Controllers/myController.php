3e7f9a370dd9fa19bb941fdd5f0f455c9cd01665
Alessandro <marzucoalessandro1@gmail.com>
2021-02-17 15:23:02 +0100

    inizio e creazione delle view e del controller

---
+++ app/Http/Controllers/myController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller
+{
+  public function home(){
+    return view('home');
+  }
+
+  public function about(){
+    return view('about');
+  }
+
+  public function blog(){
+    return view('blog');
+  }
+}
